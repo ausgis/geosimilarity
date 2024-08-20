f5627c349c9ef32b068a9b3cba935a3e6f0113c0
SpatLyu <lyu.geosocial@gmail.com>
2024-08-20 19:51:49 +0800

    add data description

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' @title Spatial datasets of trace element Zn.
+#' @description Spatial datasets of trace element Zn.
+#'
+#' @name zn
+#' @format \code{zn}: A tibble of trace element Zn with 894 rows and 12 variables
+#' @docType data
+#' @author Yongze Song \email{yongze.song@outlook.com}
+#' @keywords dataset trace elements
+"zn"
+
+#' @title Spatial grid data of explanatory variables.
+#' @description Spatial grid data of explanatory variables.
+#'
+#' @name grid
+#' @format \code{grid}: A tibble of grided trace element explanatory variables
+#' with 13132 rows and 12 variables, where the first column is ID.
+#' @docType data
+#' @author Yongze Song \email{yongze.song@outlook.com}
+#' @keywords dataset trace elements
+"grid"
--- R/gos.R
+++ R/gos.R
@@ -102,28 +102,24 @@ gos = \(formula, data = NULL, newdata = NULL, kappa = 0.25, cores = 1){
 #'
 #' @param formula A formula of GOS model
 #' @param data A data.frame or tibble of observation data
-#' @param kappa (optional)A numeric vector of the optional percentages of observation locations
+#' @param kappa (optional) A numeric vector of the optional percentages of observation locations
 #'              with high similarity to a prediction location.
 #'              kappa = 1 - tau, where tau is the probability parameter
 #'              in quantile operator. kappa = 0.25 means
 #'              that 25% of observations with high similarity to a prediction
 #'              location are used for modelling.
-#' @param nrepeat (optional)A numeric value of the number of cross-validation training times.
+#' @param nrepeat (optional) A numeric value of the number of cross-validation training times.
 #'                The default value is 10.
-#' @param nsplit (optional)The sample training set segmentation ratio,which in `(0,1)`,
+#' @param nsplit (optional) The sample training set segmentation ratio,which in `(0,1)`,
 #' default is `0.5`.
-#' @param cores positive integer(default is 1). If cores > 1, a 'parallel' package
-#' cluster with that many cores is created and used. You can also supply a cluster
-#' object.
+#' @param cores (optional) positive integer(default is 1). If cores > 1, a 'parallel' package
+#' cluster with that many cores is created and used. You can also supply a cluster object.
 #'
 #' @return A list of the result of the best kappa and the computation process curve.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' library(dplyr)
-#' library(ggplot2)
-#' library(ggrepel)
 #' data(zn)
 #' data(grid)
 #' system.time({
