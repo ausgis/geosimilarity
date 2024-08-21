67d78964c026a05a982fdcaa3bc8bbb31097fc2b
SpatLyu <lyu.geosocial@gmail.com>
2024-08-21 15:10:50 +0800

    add package startup message

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onAttach = function(...){
+  packageStartupMessage("This is `geosimilarity` 3.0.
+                        \nTo cite `geosimilarity` in publications, please use:
+                        \nSong, Y. (2022). Geographically Optimal Similarity. Mathematical Geosciences. doi: 10.1007/s11004-022-10036-8.
+                        ")
+}
