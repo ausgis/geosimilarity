10806ad300d6d81cf6ab5eb8c6c9c59bce44e6a4
SpatLyu <lyu.geosocial@gmail.com>
2024-09-09 13:19:42 +0800

    update pipe document

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,14 @@
 #' @title Pipe operator
 #' @description
 #' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
-#' @rdname pipe
 #'
 #' @name %>%
-#' @importFrom magrittr %>%
-#' @export
-#' @keywords internal
 #' @usage lhs \%>\% rhs
+#' @rdname pipe
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @return `NULL` (this is the magrittr pipe operator)
+#' @export
 NULL
 
 #' Removing outliers.
