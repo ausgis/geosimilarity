5533aaf573084199231c97e6ec09f68191d5fb1e
SpatLyu <lyu.geosocial@gmail.com>
2024-11-01 09:00:19 +0800

    update zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach = function(...){
-  packageStartupMessage("This is `geosimilarity` 3.7.
+  packageStartupMessage("This is `geosimilarity` 3.8.
                         \nTo cite `geosimilarity` in publications, please use:
                         \nSong, Y. (2022). Geographically Optimal Similarity. Mathematical Geosciences. doi: 10.1007/s11004-022-10036-8.
                         ")
